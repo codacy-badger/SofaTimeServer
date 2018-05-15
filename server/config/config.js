44679b51f034183eadfc9f230a123671355d404b
damjanh <damjan.hudobreznik@gmail.com>
2018-05-15 18:20:57 +0200

    Fix lint issues with config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 const env = process.env.NODE_ENV || 'development'; // Check if environment was set for testing or use default 'development'.
-import config from "./config.json"; // Read config file.
+import config from './config.json'; // Read config file.
 
 // Sets configs from config file to process.env.
 if (env === 'development' || env === 'test') {
-  const envConfig = config[env];
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key];
-  });
+    const envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
