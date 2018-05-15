55fd3185f6e0c9d75770aa5826a2164b8c911cd2
damjanh <damjan.hudobreznik@gmail.com>
2018-05-15 18:34:22 +0200

    Support no config file.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,15 @@
 const env = process.env.NODE_ENV || 'development'; // Check if environment was set for testing or use default 'development'.
-import config from './config.json'; // Read config file.
 
-// Sets configs from config file to process.env.
-if (env === 'development' || env === 'test') {
-    const envConfig = config[env];
-    Object.keys(envConfig).forEach((key) => {
-        process.env[key] = envConfig[key];
-    });
+try {
+    // Use require since import can only be used at top level.
+    const config = require('./config.json'); // Read config file.
+    // Sets configs from config file to process.env.
+    if (env === 'development' || env === 'test') {
+        const envConfig = config[env];
+        Object.keys(envConfig).forEach((key) => {
+            process.env[key] = envConfig[key];
+        });
+    }
+} catch (e) {
+    console.log('Config file was not pressent.');
 }
